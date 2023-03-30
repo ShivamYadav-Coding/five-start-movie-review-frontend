6532972225731b903f8e0c89c28216b2ce6d1138
Shivam Yadav <shivam.coding@gmail.com>
2023-03-30 17:02:51 +0530

    changed the base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://five-star-review-backend.onrender.com/api",
 });
 
 export default client;
